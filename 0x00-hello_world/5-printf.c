a043acb2f82b952a2fa527b7ff8db85e5436e640
datacentri <myemail@gmail.com>
2023-02-16 05:11:29 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main – Second entry point
+ * main - Entry
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
-
