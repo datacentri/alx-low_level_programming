fb112f390730c48e0ebd2cfa2a9a00ad96510b71
datacentri <myemail@gmail.com>
2023-02-21 07:27:44 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - it will print 10x alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
