fb5fcd39592edcad01caae332723d4a92c682496
datacentri <myemail@gmail.com>
2023-02-21 05:01:39 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+ #include"main.h"
+
+/**
+ * print_alphabet - printing in lower case
+ *
+ * no return
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * main - program entry
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+		return (0);
+}
