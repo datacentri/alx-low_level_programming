b81ad3d183a84d6736c114f3938e6f0e56783735
datacentri <myemail@gmail.com>
2023-02-27 08:06:20 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,49 +1,21 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer
- * @n: random integer
- * Return: void
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
  */
 void print_number(int n)
 {
-	char ld, cr;
-	int r;
-	int ct = 0;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		_putchar ('-');
-		ld = (char)('0' - (n % 10));
-		n = n / -10;
+		_putchar(45);
+		x = -n;
 	}
-
-	else
-	{
-		ld = (char)((n % 10) + '0');
-		n = n / 10;
-	}
-
-	r = 0;
-	while (n > 0)
-	{
-		r = r * 10 + (n % 10);
-		n = n / 10;
-		ct++;
-	}
-
-	while (r > 0)
-	{
-		cr = (char)((r % 10) + '0');
-		_putchar(cr);
-		r = r / 10;
-		ct--;
-	}
-	while (ct != 0)
-	{
-		_putchar('0');
-		ct--;
-	}
-	_putchar(ld);
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
-
