3b2de89a57832ec8d5b5edd1bae33bb19f5ff7e0
datacentri <myemail@gmail.com>
2023-02-27 08:03:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 
-/** 
- * main - Entry
- * Return: 1
+/**
+ * main -this wll create a write function
+ *
+ * Return: always 1 (error)
  */
-
 int main(void)
 {
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
 }
