68ae1cae8a49ad3da2ccd53ecbc744b4f2e113db
datacentri <contactdatacentri@gmail.com>
2023-04-11 04:56:31 -0700

    Datacentri 0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,26 +10,26 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
-	int len, base_two;
+	int hat, basetwo;
 
 	if (!b)
 		return (0);
 
 	ui = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (hat = 0; b[hat] != '\0'; hat++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (hat--, basetwo = 1; hat >= 0; hat--, basetwo *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[hat] != '0' && b[hat] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[hat] & 1)
 		{
-			ui += base_two;
+			ui += basetwo;
 		}
 	}
 
