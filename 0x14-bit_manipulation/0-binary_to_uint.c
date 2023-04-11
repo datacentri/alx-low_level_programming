dca4779072fffcd0fae4a729381c9dc55c4359a3
datacentri <contactdatacentri@gmail.com>
2023-04-11 04:03:07 -0700

    Datacentri 0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts binary number to an unsigned int
- * @b: a string of 0 and 1 chars
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: converted number or 0 if b is NULL or different from 0 or 1
+ * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int length, i, add;
+	unsigned int ui;
+	int len, base_two;
 
-	add = 0;
-
-	if (b != NULL)
-	{
-		for (length = 0 ; b[length] != '\0' ; length++)
-		{
-			if (b[length] != 48 && b[length] != 49)
-			{
-				return (0);
-			}
-		}
-
-		for (i = 0, length-- ; length >= 0 ; length--)
-		{
-			add = add + ((b[length] - '0') << i);
-			i++;
-		}
-	}
-	else
-	{
+	if (!b)
 		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (add);
+
+	return (ui);
 }
