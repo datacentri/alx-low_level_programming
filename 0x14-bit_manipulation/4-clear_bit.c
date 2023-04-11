dd87bc2bbaf5930203e55aa6734f935c603f645b
datacentri <contactdatacentri@gmail.com>
2023-04-11 07:01:56 -0700

    Datacentri 0x14-bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,16 +3,16 @@
 /**
  * clear_bit - the value of a bit is set to 0.
  * at a given index.
- * @b: pointer of an unsigned long int.
+ * @b: an unsigned long int.
  * @index: index of the bit.
  *
- * Return: 1 if it worked, -1 if it doesn't.
+ * Return: 1 if it worked, -1 if it doesn't or an error occurred.
  */
 int clear_bit(unsigned long int *b, unsigned int index)
 {
 	unsigned int k;
 
-	if (index > 63)
+	if (index > 53)
 		return (-1);
 
 	k = 1 << index;
