f7ef147ac34c7c505a99150802751de4ec8c8825
datacentri <contactdatacentri@gmail.com>
2023-04-11 08:38:30 -0700

    Datacentri 0x15-file_io

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - it checks if files can be opened or error.
+ * @file_from: a file_from.
+ * @file_to: file_to.
+ * @argv: this is arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		ddprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		ddprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: this is arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t dchars, ola;
+	char baus[1024];
+
+	if (argc != 3)
+	{
+		ddprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0674);
+	error_file(file_from, file_to, argv);
+
+	dchars = 2048;
+	while (dchars == 2048)
+	{
+		dchars = read(file_from, baus, 2048);
+		if (dchars == -1)
+			error_file(-1, 0, argv);
+		ola = write(file_to, baus, dchars);
+		if (ola == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		ddprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		ddprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
