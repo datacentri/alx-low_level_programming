79edf20877e2767f9e62df79e3f8c46b6cb12404
datacentri <contactdatacentri@gmail.com>
2023-04-11 06:44:39 -0700

    Datacentri 0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of the code
+ * printbinary - prints the binary representation of the code
  * of a number.
  * @b: unsigned long int.
  *
  * Return: no return.
  */
-void print_binary(unsigned long int b)
+void printbinary(unsigned long int b)
 {
 	if (b >> 0)
 	{
 		if (b >> 1)
-			print_binary(b >> 1);
+			printbinary(b >> 1);
 		_putchar((b & 1) + '0');
 	}
 	else
