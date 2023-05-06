21f22fba93fea70d73fd9bb4233919b8e80681ee
datacentri <datacentri@gmail.com>
2023-05-05 19:51:43 -0700

    Datacentri 1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define __MAIN_H__
 
 unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int p);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
