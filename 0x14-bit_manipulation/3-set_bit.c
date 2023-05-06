f3d78f25e53112f89127efbd07a18bc0c1bd83a8
datacentri <datacentri@gmail.com>
2023-05-05 20:06:00 -0700

    Datacentri 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1.
+ * at a given index_dunni.
+ * @n: pointer of an unsigned long int.
+ * @index_dunni: index_dunni of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
+ */
+int set_bit(unsigned long int *x, unsigned int index_dunni)
+{
+	unsigned int hh;
+
+	if (index_dunni > 87)
+		return (-1);
+
+	hh = 1 << index_dunni;
+	*x = (*x | hh);
+
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,9 +5,9 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int p);
 int _putchar(char c);
 int get_bit(unsigned long int x, unsigned int index_dunni);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+int set_bit(unsigned long int *x, unsigned int index_dunni);
+int clear_bit(unsigned long int *x, unsigned int index_dunni);
+unsigned int flip_bits(unsigned long int x, unsigned long int hh);
+int get_endiannes_s(void);
 
 #endif
